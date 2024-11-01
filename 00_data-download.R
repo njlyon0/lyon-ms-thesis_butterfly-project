f98c60d04fb235da8e8ba30e739cc4f727308024
njlyon0 <njlyon@alumni.iastate.edu>
2024-11-01 15:23:29 -0400

    feat: added option to forgo redownloading of data if desired (default is to redownload)

--- 00_data-download.R
+++ 00_data-download.R
@@ -36,9 +36,15 @@ tidy_data <- googledrive::drive_ls(path = tidy_url)
 # Identify only the needed files
 needed_data <- tidy_data %>% 
   ## Pare down to only data relevant to this project
-  dplyr::filter(stringr::str_detect(string = name, pattern = "butterfly-project_")) %>% 
-  ## And remove already downloaded files
-  dplyr::filter(!name %in% already_done)
+  dplyr::filter(stringr::str_detect(string = name, pattern = "butterfly-project_"))
+
+# Is redownloading desired?
+redownload <- TRUE
+
+# # Remove already downloaded files if re-downloading isn't desired
+if(redownload == F){
+  needed_data <- dplyr::filter(needed_data, !name %in% already_done) 
+}
 
 # Download the needed files
 purrr::walk2(.x = needed_data$id, .y = needed_data$name,
