ca2c2c3bc34cb7afd723621eb835111ab44ef9ec
njlyon0 <njlyon@alumni.iastate.edu>
2024-11-01 17:03:44 -0400

    feat (stats): univariate statistics written

--- 02_statistics.R
+++ 02_statistics.R
@@ -12,7 +12,7 @@
 
 # Set required libraries
 # install.packages("librarian")
-librarian::shelf(tidyverse)
+librarian::shelf(tidyverse, lmerTest)
 
 # Clear environment & collect garbage
 rm(list = ls()); gc()
@@ -20,17 +20,77 @@ rm(list = ls()); gc()
 # Create needed folder(s)
 dir.create(path = file.path("results"), showWarnings = F)
 
+# Load desired custom function(s)
+source(file.path("tools", "fxn_tabularize-mem-results.R"))
+
+# Read in butterfly & floral data
+bf <- read.csv(file = file.path("data", "ready-butterflies.csv"))
+flr <- read.csv(file = file.path("data", "ready-flowers.csv"))
+
+# Check structure
+dplyr::glimpse(bf)
+dplyr::glimpse(flr)
+
 ##  ------------------------------------------  ##      
-# Univariate ----
+                # Univariate ----
 ##  ------------------------------------------  ##      
 
+# Make an empty list
+results_list <- list()
 
+# Identify the three community metrics we're interested in (for now)
+metrics <- c("abundance", "richness", "diversity_shannon")
 
+# Loop across desired response variables
+for(var in c(paste0("butterfly.", metrics), paste0("flower.", metrics))){
+  
+  # Progress message
+  message("Getting results for ", var)
+  
+  # Fit model
+  if(stringr::str_detect(string = var, pattern = "butterfly")){
+    mem <- lmerTest::lmer(bf[[var]] ~ adaptive.mgmt * year + (1|pasture), data = bf)
+  } else {
+    mem <- lmerTest::lmer(flr[[var]] ~ adaptive.mgmt * year + (1|pasture), data = flr)
+  }
+  
+  # Extract the ANOVA table of results
+  var_result <- tabularize_mem_results(mod = mem) %>% 
+    dplyr::mutate(response = var, .before = dplyr::everything())
+  
+  # If the interaction is non-significant...
+  if(dplyr::filter(var_result, term == "adaptive.mgmt:year")$p.value > 0.1){
+    
+    # Fit simpler model (no ixn term)
+    if(stringr::str_detect(string = var, pattern = "butterfly")){
+      mem <- lmerTest::lmer(bf[[var]] ~ adaptive.mgmt + year + (1|pasture), data = bf)
+    } else {
+      mem <- lmerTest::lmer(flr[[var]] ~ adaptive.mgmt + year + (1|pasture), data = flr)
+    }
+    
+    # Re-extract ANOVA table
+    var_result <- tabularize_mem_results(mod = mem) %>% 
+      dplyr::mutate(response = var, .before = dplyr::everything())
+  }
+  
+  # Add results to the results list
+  results_list[[var]] <- var_result
+  
+}
 
+# Unlist & wrangle the output
+results_df <- results_list %>% 
+  purrr::list_rbind(x = .) %>%
+  dplyr::mutate(f.statistic = round(x = f.statistic, digits = 1),
+                p.value = round(x = p.value, digits = 3))
 
+# Check that out
+dplyr::glimpse(results_df)
+## view(results_df)
 
-
-
+# Export the results
+write.csv(x = results_df, row.names = F, na = '',
+          file = file.path("results", "univariate-stats.csv"))
 
 ##  ------------------------------------------  ##      
 # Multivariate ----
