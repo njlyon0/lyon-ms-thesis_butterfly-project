ffe768b1ca2cd7d61a8e1d07ea9b9c8d6802f6c4
njlyon0 <njlyon@alumni.iastate.edu>
2024-11-01 15:39:52 -0400

    feat (stats): script skeleton with likely headers

---
+++ 02_statistics.R
@@ -0,0 +1,42 @@
+##  ------------------------------------------------------------  ##
+           # Butterfly Project - Statistical Analysis
+##  ------------------------------------------------------------  ##
+# Written by Nick J Lyon
+
+# Script purpose:
+## Perform statistical analysis to assess hypotheses
+
+##  ------------------------------------------  ##      
+                # Housekeeping ----
+##  ------------------------------------------  ##      
+
+# Set required libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse)
+
+# Clear environment & collect garbage
+rm(list = ls()); gc()
+
+# Create needed folder(s)
+dir.create(path = file.path("results"), showWarnings = F)
+
+##  ------------------------------------------  ##      
+# Univariate ----
+##  ------------------------------------------  ##      
+
+
+
+
+
+
+
+
+
+##  ------------------------------------------  ##      
+# Multivariate ----
+##  ------------------------------------------  ##      
+
+
+
+
+# End ----
