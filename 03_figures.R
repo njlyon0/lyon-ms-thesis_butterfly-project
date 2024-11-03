e96abfd80099243d0b2922b889f7dd9faf842489
njlyon0 <njlyon@alumni.iastate.edu>
2024-11-03 12:53:46 -0500

    style (viz): made year x-axis breaks cleaner

--- 03_figures.R
+++ 03_figures.R
@@ -60,7 +60,8 @@ mgmt.shapes <- c("GB" = 24, "GB-IC" = 25, "IC" = 23, "BO" = 21, "PBG" = 22)
 ## By year
 flr.abun1 <- make_fig(df = flr, resp = "flower.abundance", focus = "year", 
                       sig = FALSE, cols = mgmt.cols, shps = mgmt.shps) +
-  ylim(0, (3*10^4))
+  ylim(0, (3*10^4)) +
+  scale_x_continuous(breaks = seq(from = 8, to = 18, by = 2))
   
 ## By management
 flr.abun2 <- make_fig(df = flr, resp = "flower.abundance", focus = "mgmt", 
@@ -78,6 +79,7 @@ flr.abun
 # Flower Richness
 flr.rich <- make_fig(df = flr, resp = "flower.richness", focus = "ixn", 
                      cols = mgmt.cols, shps = mgmt.shps) +
+  scale_x_continuous(breaks = seq(from = 8, to = 18, by = 2)) +
   theme(legend.position = "inside",
         legend.position.inside = c(0.15, 0.8))
 flr.rich
@@ -87,7 +89,8 @@ flr.rich
 flr.dive1 <- make_fig(df = flr, resp = "flower.diversity_shannon", focus = "year", 
                       sig = FALSE, cols = mgmt.cols, shps = mgmt.shps) +
   labs(y = "Flower Diversity") +
-  ylim(0, 3.2)
+  ylim(0, 3.2) +
+  scale_x_continuous(breaks = seq(from = 8, to = 18, by = 2))
 
 ## By management
 flr.dive2 <- make_fig(df = flr, resp = "flower.diversity_shannon", focus = "mgmt", 
@@ -104,7 +107,7 @@ flr.dive
 # Assemble into a multi-panel figure
 cowplot::plot_grid(flr.abun, flr.rich, flr.dive, labels = "AUTO", ncol = 1)
 ggsave(filename = file.path("figures", "figure_flower.png"),
-       width = 4, height = 12, units = "in")
+       width = 5, height = 12, units = "in")
 
 ##  ------------------------------------------  ##
         # Univariate - Butterflies ----
@@ -113,6 +116,7 @@ ggsave(filename = file.path("figures", "figure_flower.png"),
 # Butterfly Abundance
 bf.abun <- make_fig(df = bf, resp = "butterfly.abundance", focus = "ixn", 
                     cols = mgmt.cols, shps = mgmt.shps) +
+  scale_x_continuous(breaks = seq(from = 8, to = 18, by = 2)) +
   theme(legend.position = "inside",
         legend.position.inside = c(0.15, 0.8))
 bf.abun
@@ -121,7 +125,8 @@ bf.abun
 ## By year
 bf.rich1 <- make_fig(df = bf, resp = "butterfly.richness", focus = "year", 
                      cols = mgmt.cols, shps = mgmt.shps) +
-  ylim(5, 25)
+  ylim(5, 25) +
+  scale_x_continuous(breaks = seq(from = 8, to = 18, by = 2))
 
 ## By management
 bf.rich2 <- make_fig(df = bf, resp = "butterfly.richness", focus = "mgmt", 
@@ -139,12 +144,13 @@ bf.rich
 bf.dive <- make_fig(df = bf, resp = "butterfly.diversity_shannon", focus = "ixn", 
                     cols = mgmt.cols, shps = mgmt.shps) +
   labs(y = "Butterfly Diversity") +
+  scale_x_continuous(breaks = seq(from = 8, to = 18, by = 2)) +
   theme(legend.position = "none")
 bf.dive
 
 # Assemble into a multi-panel figure
 cowplot::plot_grid(bf.abun, bf.rich, bf.dive, labels = "AUTO", ncol = 1)
 ggsave(filename = file.path("figures", "figure_butterfly.png"),
-       width = 4, height = 12, units = "in")
+       width = 5, height = 12, units = "in")
 
 # End ----
