cf863405b383cf28548d8e1850f19e1f0a127171
njlyon0 <njlyon@alumni.iastate.edu>
2026-05-21 11:36:39 -0400

    feat (visit): add conditional identification of core management variables

--- 03_visit-wrangle.r
+++ 03_visit-wrangle.r
@@ -103,7 +103,8 @@ dplyr::glimpse(vst_v04)
 
 # Remove any rows/columns that are not likely to be useful
 vst_v05 <- vst_v04 %>% 
-  dplyr::filter(!site %in% c("ACCIDENTAL ROW")) %>% 
+  dplyr::filter(!site %in% c("ACCIDENTAL ROW")) %>%
+  dplyr::filter(!is.na(year)) %>% 
   dplyr::select(-month, -round, -dplyr::ends_with("_time"), -dplyr::contains("data.ent"), -comments)
 
 # What was lost?
@@ -132,11 +133,91 @@ vst_v06 <- vst_v05 %>%
 dplyr::glimpse(vst_v06)
 
 ##  ------------------------------------------  ##
+# Identify Management/Treatment Information ----
+##  ------------------------------------------  ##
+
+# Identify a number of critical treatments over the study period
+vst_v07 <- vst_v06 %>% 
+  dplyr::mutate(management_pyric.herbivory = dplyr::case_when(
+    site %in% c("KLT", "RIN", "PAW") ~ "burn only",
+    year < 2014 & site == "RCH" ~ "burn only",
+    site %in% c("GIL", "LTR", "PYW", "STE") ~ "graze and burn",
+    site %in% c("235", "KLN", "PYN", "PYS", "RIS") ~ "patch-burn graze",
+    year >= 2014 & site == "RCH" ~ "patch-burn graze",
+    site %in% c("BSH", "DUN", "RC2") ~ "none",
+    TRUE ~ "unknown"), .after = site) %>% 
+  dplyr::mutate(management_anti.fescue = dplyr::case_when(
+    year <= 2013 ~ NA,
+    patch %in% c("BSH-N", "DUN-C", "GIL-S", "LTR-W", "PYW-S", "RC2-S", "STE-W") ~ "control",
+    patch %in% c("BSH-C", "DUN-E", "GIL-N", "LTR-C", "PYW-C", "RC2-C", "STE-N") ~ "herbicide only",
+    patch %in% c("BSH-S", "DUN-W", "GIL-C", "LTR-E", "PYW-N", "RC2-N", "STE-S") ~ "herbicide and seedmix addition",
+    TRUE ~ NA), .after = management_pyric.herbivory) %>% 
+  dplyr::mutate(burn.cohort = dplyr::case_when(
+    patch %in% c("KLN-W", "PYN-S", "PYS-N", "RIS-C") ~ "A",
+    patch %in% c("KLN-C", "PYN-W", "PYS-S", "RIS-N") ~ "B",
+    patch %in% c("KLN-E", "PYN-N", "PYS-W", "RIS-S") ~ "C",
+    site %in% c("LTR", "PYW") ~ "A",
+    TRUE ~ NA)) %>% 
+  dplyr::mutate(time.since.fire_years = dplyr::case_when(
+    burn.cohort == "A" & year %in% (2000 + c(9, 12, 15, 18)) ~ "0",
+    burn.cohort == "A" & year %in% (2000 + c(10, 13, 16)) ~ "1",
+    burn.cohort == "A" & year %in% (2000 + c(11, 14, 17)) ~ "2",
+    burn.cohort == "B" & year %in% (2000 + c(8, 11, 14, 17)) ~ "0",
+    burn.cohort == "B" & year %in% (2000 + c(9, 12, 15, 18)) ~ "1",
+    burn.cohort == "B" & year %in% (2000 + c(10, 13, 16)) ~ "2",
+    burn.cohort == "C" & year %in% (2000 + c(7, 10, 13, 16)) ~ "0",
+    burn.cohort == "C" & year %in% (2000 + c(8, 11, 14, 17)) ~ "1",
+    burn.cohort == "C" & year %in% (2000 + c(9, 12, 15, 18)) ~ "2",
+    site == "GIL" & year %in% (2000 + c(9, 12, 15)) ~ "0",
+    site == "GIL" & year %in% (2000 + c(10, 13, 16)) ~ "1",
+    site == "GIL" & year %in% (2000 + c(11, 14, 17)) ~ "2",
+    site == "GIL" & year == 2018 ~ "3",
+    site %in% c("KLT", "RIN") & year %in% (2000 + c(9, 12)) ~ "0",
+    site %in% c("KLT", "RIN") & year %in% (2000 + c(10, 13)) ~ "1",
+    site %in% c("KLT", "RIN") & year %in% (2000 + c(11, 14)) ~ "2",
+    site %in% c("KLT", "RIN") & year == 2015 ~ "3",
+    site == "KLT" & year == 2016 ~ "0",
+    site == "KLT" & year == 2017 ~ "1",
+    site == "KLT" & year == 2018 ~ "2",
+    site == "RIN" & year == 2016 ~ "4",
+    site == "RIN" & year == 2017 ~ "0",
+    site == "RIN" & year == 2018 ~ "1",
+    site == "PAW" & year %in% (2000 + c(8, 13, 17)) ~ "0",
+    site == "PAW" & year %in% (2000 + c(9, 14, 18)) ~ "1",
+    site == "PAW" & year %in% (2000 + c(7, 10, 15)) ~ "2",
+    site == "PAW" & year %in% (2000 + c(11, 16)) ~ "3",
+    site == "PAW" & year %in% (2000 + c(12)) ~ "4",
+    TRUE ~ "unknown"), .after = patch) %>% 
+  dplyr::select(-burn.cohort)
+
+# Check management entries
+vst_v07 %>% 
+  dplyr::group_by(management_pyric.herbivory) %>% 
+  dplyr::summarize(sites = paste(sort(unique(site)), collapse = "; "),
+    .groups = "drop")
+
+# Check anti-fescue management entries
+vst_v07 %>% 
+  dplyr::filter(year >= 2014) %>% 
+  dplyr::group_by(management_anti.fescue) %>% 
+  dplyr::summarize(sites = paste(sort(unique(site)), collapse = "; "),
+    .groups = "drop")
+
+# Check TSF entries
+vst_v07 %>% 
+  dplyr::select(patch, year, time.since.fire_years) %>% 
+  dplyr::distinct() %>% 
+  tidyr::pivot_wider(names_from = year, values_from = time.since.fire_years)
+
+# Check structure
+dplyr::glimpse(vst_v07)
+
+##  ------------------------------------------  ##
 # Export ----
 ##  ------------------------------------------  ##
 
 # Make a final object
-vst_v99 <- vst_v06
+vst_v99 <- vst_v07
 
 # Check structure
 dplyr::glimpse(vst_v99)
