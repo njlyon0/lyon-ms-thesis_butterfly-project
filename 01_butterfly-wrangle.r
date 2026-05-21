17775d19165c515f0aa63398fa44f3fee37d02b3
njlyon0 <njlyon@alumni.iastate.edu>
2026-05-21 08:41:55 -0400

    feat (bfly): add script skeleton for butterfly wrangling

---
+++ 01_butterfly-wrangle.r
@@ -0,0 +1,22 @@
+##  ------------------------------------------------------------  ##
+# Butterfly Project - Wrangle Butterfly Data
+##  ------------------------------------------------------------  ##
+# Purpose:
+## Wrangle butterfly data (e.g., do quality control, calculate metrics)
+
+# Load needed libraries
+# install.packages("librarian")
+librarian::shelf(tidyverse, supportR)
+
+# Get set up
+source("-setup.r")
+
+# Clear environment
+rm(list = ls()); gc()
+
+##  ------------------------------------------  ##
+# Load Data ----
+##  ------------------------------------------  ##
+
+
+# End ----
