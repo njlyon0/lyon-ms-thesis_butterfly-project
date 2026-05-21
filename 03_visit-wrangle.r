cc72b4463925f36146b4c6faed1cb987fbc5c1c0
njlyon0 <njlyon@alumni.iastate.edu>
2026-05-21 12:17:45 -0400

    feat (visit): add more conditional mgmt identification

--- 03_visit-wrangle.r
+++ 03_visit-wrangle.r
@@ -76,8 +76,18 @@ sort(unique(vst_v03$round))
 sort(unique(vst_v03$butterfly_observer))
 sort(unique(vst_v03$nectar_observer))
 
+# Check patches within site
+vst_v03 %>% 
+  dplyr::group_by(site) %>% 
+  dplyr::summarize(patch_ct = length(unique(patch)),
+    patches = paste(unique(patch), collapse = "; "),
+    .groups = "drop") %>% 
+  dplyr::filter(patch_ct != 3)
+
 # Standardize and streamline those columns
 vst_v04 <- vst_v03 %>% 
+  dplyr::mutate(dplyr::across(.cols = site:patch,
+    .fns = ~ gsub(pattern = "RIN-C", replacement = "RIN-S", x = .))) %>% 
   dplyr::mutate(butterfly_observer = dplyr::case_when(
     butterfly_observer %in% c("???") ~ "unrecorded",
     butterfly_observer %in% c("rayy moranz") ~ "ray moranz",
@@ -145,19 +155,19 @@ vst_v07 <- vst_v06 %>%
     site %in% c("235", "KLN", "PYN", "PYS", "RIS") ~ "patch-burn graze",
     year >= 2014 & site == "RCH" ~ "patch-burn graze",
     site %in% c("BSH", "DUN", "RC2") ~ "none",
-    TRUE ~ "unknown"), .after = site) %>% 
+    TRUE ~ "unknown"), .after = patch) %>% 
   dplyr::mutate(management_anti.fescue = dplyr::case_when(
-    year <= 2013 ~ NA,
+    year <= 2013 ~ "none",
     patch %in% c("BSH-N", "DUN-C", "GIL-S", "LTR-W", "PYW-S", "RC2-S", "STE-W") ~ "control",
     patch %in% c("BSH-C", "DUN-E", "GIL-N", "LTR-C", "PYW-C", "RC2-C", "STE-N") ~ "herbicide only",
     patch %in% c("BSH-S", "DUN-W", "GIL-C", "LTR-E", "PYW-N", "RC2-N", "STE-S") ~ "herbicide and seedmix addition",
-    TRUE ~ NA), .after = management_pyric.herbivory) %>% 
+    TRUE ~ "none"), .after = management_pyric.herbivory) %>% 
   dplyr::mutate(burn.cohort = dplyr::case_when(
     patch %in% c("KLN-W", "PYN-S", "PYS-N", "RIS-C") ~ "A",
     patch %in% c("KLN-C", "PYN-W", "PYS-S", "RIS-N") ~ "B",
     patch %in% c("KLN-E", "PYN-N", "PYS-W", "RIS-S") ~ "C",
     site %in% c("LTR", "PYW") ~ "A",
-    TRUE ~ NA)) %>% 
+    TRUE ~ "X")) %>% 
   dplyr::mutate(time.since.fire_years = dplyr::case_when(
     burn.cohort == "A" & year %in% (2000 + c(9, 12, 15, 18)) ~ "0",
     burn.cohort == "A" & year %in% (2000 + c(10, 13, 16)) ~ "1",
@@ -168,6 +178,15 @@ vst_v07 <- vst_v06 %>%
     burn.cohort == "C" & year %in% (2000 + c(7, 10, 13, 16)) ~ "0",
     burn.cohort == "C" & year %in% (2000 + c(8, 11, 14, 17)) ~ "1",
     burn.cohort == "C" & year %in% (2000 + c(9, 12, 15, 18)) ~ "2",
+    patch == "235-C" & year == 2016 ~ "0",
+    patch == "235-C" & year %in% (2000 + c(14, 17)) ~ "1",
+    patch == "235-C" & year %in% (2000 + c(15, 18)) ~ "2",
+    patch == "235-N" & year %in% (2000 + c(14, 17)) ~ "0",
+    patch == "235-N" & year %in% (2000 + c(15, 18)) ~ "1",
+    patch == "235-N" & year == 2016 ~ "2",
+    patch == "235-S" & year %in% (2000 + c(15, 18)) ~ "0",
+    patch == "235-S" & year == 2016 ~ "1",
+    patch == "235-S" & year %in% (2000 + c(14, 17)) ~ "2",
     site == "GIL" & year %in% (2000 + c(9, 12, 15)) ~ "0",
     site == "GIL" & year %in% (2000 + c(10, 13, 16)) ~ "1",
     site == "GIL" & year %in% (2000 + c(11, 14, 17)) ~ "2",
@@ -187,8 +206,29 @@ vst_v07 <- vst_v06 %>%
     site == "PAW" & year %in% (2000 + c(7, 10, 15)) ~ "2",
     site == "PAW" & year %in% (2000 + c(11, 16)) ~ "3",
     site == "PAW" & year %in% (2000 + c(12)) ~ "4",
-    TRUE ~ "unknown"), .after = patch) %>% 
+    site == "RCH" & year %in% (2000 + c(9, 12)) ~ "0",
+    site == "RCH" & year %in% (2000 + c(10, 13)) ~ "1",
+    site == "RCH" & year == 2011 ~ "2",
+    patch == "RCH-C" & year %in% (2000 + c(15, 18)) ~ "0",
+    patch == "RCH-C" & year == 2016 ~ "1",
+    patch == "RCH-C" & year %in% (2000 + c(14, 17)) ~ "2",
+    patch == "RCH-E" & year == 2016 ~ "0",
+    patch == "RCH-E" & year == 2017 ~ "1",
+    patch == "RCH-E" & year %in% (2000 + c(14, 18)) ~ "2",
+    patch == "RCH-E" & year == 2015 ~ "3",
+    patch == "RCH-W" & year %in% (2000 + c(14, 17)) ~ "0",
+    patch == "RCH-W" & year %in% (2000 + c(15, 18)) ~ "1",
+    patch == "RCH-W" & year == 2016 ~ "2",
+    site == "STE" & year %in% (2000 + c(9, 12, 16)) ~ "0",
+    site == "STE" & year %in% (2000 + c(10, 13, 17)) ~ "1",
+    site == "STE" & year %in% (2000 + c(11, 14, 18)) ~ "2",
+    site == "STE" & year == 2015 ~ "3",
+    TRUE ~ "unknown"), .after = management_anti.fescue) %>% 
   dplyr::select(-burn.cohort)
+
+vst_v07 %>% 
+  filter(year >= 2014 & site == "RCH") %>% 
+  select(site, patch) %>% distinct()
 
 # Check management entries
 vst_v07 %>% 
@@ -204,10 +244,11 @@ vst_v07 %>%
     .groups = "drop")
 
 # Check TSF entries
-vst_v07 %>% 
+(check_tsf <- vst_v07 %>% 
   dplyr::select(patch, year, time.since.fire_years) %>% 
   dplyr::distinct() %>% 
-  tidyr::pivot_wider(names_from = year, values_from = time.since.fire_years)
+  tidyr::pivot_wider(names_from = year, values_from = time.since.fire_years))
+# tibble::view(check_tsf)
 
 # Check structure
 dplyr::glimpse(vst_v07)
